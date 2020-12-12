d86b4df1625fbcd8186b0456af0338701f571ae6
David Revay <daverevay@gmail.com>
2020-12-12 14:21:06 +1100

    Fix mobile display

--- components/layout.js
+++ components/layout.js
@@ -88,6 +88,7 @@ export default function Layout({ children, home }) {
           <meta name="og:title" content={siteTitle} />
           <meta name="twitter:card" content="summary_large_image" />
           <link rel="stylesheet" href="https://use.typekit.net/kuw5pxv.css"></link>
+          <meta name="viewport" content="width=700" />
         </Head>
         <header>
           <Logo src="/images/logo.png" />
